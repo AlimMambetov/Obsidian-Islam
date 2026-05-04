5f48d63a786aa6d4fe6bdb45efb1c3577b6349f1
Alim Mambetov <alim.mambetov.8866@gmail.com>
2026-05-04 15:17:40 +0300

    vercel fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -88,10 +88,10 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
-      // Plugin.Favicon(),
+      Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      // Plugin.CustomOgImages(),
+      Plugin.CustomOgImages(),
     ],
   },
 }
